d2fda3d5b09f2a6eaab350b27456134846631daa
Ashwin Srinath <shwina@users.noreply.github.com>
2020-06-04 10:59:04 -0400

    Update copyrights

--- python/nvtx/_lib/lib.pyx
+++ python/nvtx/_lib/lib.pyx
@@ -1,4 +1,8 @@
-# Copyright (c) 2020, NVIDIA CORPORATION.
+# Copyright 2020  NVIDIA Corporation.  All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 from libc.stdint cimport uint32_t
 
--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,9 @@
-# Copyright (c) 2018, NVIDIA CORPORATION.
+# Copyright 2020  NVIDIA Corporation.  All rights reserved.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 import os
 import shutil
 import sysconfig
