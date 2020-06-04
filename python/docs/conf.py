0f30319fb4596a9c4671945bff30fb2d11d7bd40
Ashwin Srinath <shwina@users.noreply.github.com>
2020-06-04 11:05:26 -0400

    Update versions to 0.1.0

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, NVIDIA Corporation"
 author = "NVIDIA Corporation"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
