db906820bdcdf40aa9c64ab9ab7821fe3765739c
Jake Hemstad <jhemstad@nvidia.com>
2020-06-15 13:55:39 -0500

    Initialize reserved portion of event attributes struct.

--- cpp/nvtx3.hpp
+++ cpp/nvtx3.hpp
@@ -1557,6 +1557,7 @@ class event_attributes {
         NVTX_COLOR_UNKNOWN,             // color type
         0,                              // color value
         NVTX_PAYLOAD_UNKNOWN,           // payload type
+        0,                              // reserved 4B
         0,                              // payload value (union)
         NVTX_MESSAGE_UNKNOWN,           // message type
         0                               // message value (union)
