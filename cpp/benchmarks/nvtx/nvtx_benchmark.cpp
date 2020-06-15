93698a17e00b646f821b67e7f940e402876a21ef
Jake Hemstad <jhemstad@nvidia.com>
2020-06-15 11:10:08 -0500

    Add benchmark comparing RAII against raw C calls.

---
+++ cpp/benchmarks/nvtx/nvtx_benchmark.cpp
@@ -0,0 +1,21 @@
+
+#include <benchmark/benchmark.h>
+
+#include <nvtx3.hpp>
+
+static void BM_global_range(::benchmark::State& state)
+{
+  nvtx3::event_attributes attr {};
+  for (auto _ : state) { nvtx3::thread_range r{attr}; }
+}
+BENCHMARK(BM_global_range);
+
+static void BM_C_global_range(::benchmark::State& state)
+{
+  nvtx3::event_attributes attr{};
+  for (auto _ : state) {
+    nvtxDomainRangePushEx(nullptr, attr.get());
+    nvtxDomainRangePop(nullptr);
+  }
+}
+BENCHMARK(BM_C_global_range);
