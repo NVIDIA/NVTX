844a3af68e84b325fe6637b0fe728c86530795c0
Ashwin Srinath <shwina@users.noreply.github.com>
2021-02-04 11:08:57 -0500

    Bump to 0.2.3

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, NVIDIA Corporation"
 author = "NVIDIA Corporation"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.2"
+release = "0.2.3"
 
 
 # -- General configuration ---------------------------------------------------
--- python/setup.py
+++ python/setup.py
@@ -32,7 +32,7 @@ extensions = [
 
 setup(
     name="nvtx",
-    version="0.2.2",
+    version="0.2.3",
     description="PyNVTX - Python code annotation library",
     url="https://github.com/NVIDIA/nvtx",
     author="NVIDIA Corporation",
