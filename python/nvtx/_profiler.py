ff0466d06eb00a0de73ca28eda75dcf0797d48c8
Ashwin Srinath <shwina@users.noreply.github.com>
2021-03-31 18:06:53 -0400

    Fix  for profiler

--- python/nvtx/_profiler.py
+++ python/nvtx/_profiler.py
@@ -3,21 +3,6 @@ import os
 from nvtx import push_range, pop_range
 
 
-def _message(frame, arg, event, annotate_cfuncs, linenos):
-    message = None
-    if event  == "call":
-        name = frame.f_code.co_name
-        if linenos:
-            fname = frame.f_code.co_filename
-            lineno = frame.f_lineno
-            message = f"{fname}:{lineno}(fname)"
-        else:
-            message = name
-    elif event == "c_call" and annotate_cfuncs:
-        message = arg.__name__
-    return message
-
-
 def profiler(linenos=True, annotate_cfuncs=False):
     """
     Parameters
@@ -29,17 +14,21 @@ def profiler(linenos=True, annotate_cfuncs=False):
     """
     def profile(frame, event, arg):
         # profile function meant to be used with sys.setprofile
-        if event in {"call", "c_call"}:
-            push_range(
-                message=_message(
-                    frame,
-                    arg,
-                    event,
-                    annotate_cfuncs=annotate_cfuncs,
-                    linenos=linenos
-                )
-            )
-        elif event in {"return", "c_return", "c_exception"}:
+        if event == "call":
+            name = frame.f_code.co_name
+            if linenos:
+                fname = os.path.basename(frame.f_code.co_filename)
+                lineno = frame.f_lineno
+                message = f"{fname}:{lineno}({name})"
+            else:
+                message = name
+            push_range(message)
+        elif event == "c_call" and annotate_cfuncs:
+            message = arg.__name__
+            push_range(message)
+        elif event == "return":
+            pop_range()
+        elif event in {"c_return", "c_exception"} and annotate_cfuncs:
             pop_range()
         return None
     return profile
