1e2389b1453cb2d2bf8e2ef9ceb5ac66fa188fa0
Ashwin Srinath <shwina@users.noreply.github.com>
2024-02-15 16:32:22 -0500

    0.2.10 bump

--- python/setup.py
+++ python/setup.py
@@ -60,7 +60,7 @@ extensions += cythonize(
 
 setup(
     name="nvtx",
-    version="0.2.9",
+    version="0.2.10",
     description="PyNVTX - Python code annotation library",
     url="https://github.com/NVIDIA/nvtx",
     author="NVIDIA Corporation",
