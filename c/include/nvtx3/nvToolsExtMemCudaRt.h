6b7c231b9f33250608d378b4ebc7fce87be04eca
Evan Ramos <evanr@nvidia.com>
2024-09-10 18:02:54 -0500

    Update license header text

--- c/include/nvtx3/nvToolsExtMemCudaRt.h
+++ c/include/nvtx3/nvToolsExtMemCudaRt.h
@@ -1,10 +1,22 @@
 /*
-* Copyright 2009-2020  NVIDIA Corporation.  All rights reserved.
-*
-* Licensed under the Apache License v2.0 with LLVM Exceptions.
-* See https://llvm.org/LICENSE.txt for license information.
-* SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-*/
+ * SPDX-FileCopyrightText: Copyright (c) 2009-2020 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Licensed under the Apache License v2.0 with LLVM Exceptions.
+ * See LICENSE.txt for license information.
+ */
 #ifndef NVTOOLSEXTV3_MEM_CUDART_V1
 #define NVTOOLSEXTV3_MEM_CUDART_V1
 
