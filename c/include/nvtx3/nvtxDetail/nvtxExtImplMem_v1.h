07b919e4805660d7c7f7716de1ad2f720b1cd691
Evan Ramos <evanr@nvidia.com>
2025-04-02 15:04:53 -0500

    Extensions: Add NVTX_SET_NAME_MANGLING_OPTIONS
    Follow-up-to: 2ce2d448f771ba96720f2327d46923a4f010647d

--- c/include/nvtx3/nvtxDetail/nvtxExtImplMem_v1.h
+++ c/include/nvtx3/nvtxDetail/nvtxExtImplMem_v1.h
@@ -40,6 +40,7 @@ extern "C" {
 
 #define NVTX_EXT_MEM_IMPL_FN_V1(ret_type, fn_name, signature, arg_names) \
 ret_type fn_name signature { \
+    NVTX_SET_NAME_MANGLING_OPTIONS \
     NVTX_EXT_HELPER_UNUSED_ARGS arg_names \
     NVTX_EXT_FN_RETURN_INVALID(ret_type) \
 }
@@ -87,6 +88,7 @@ NVTX_LINKONCE_DEFINE_FUNCTION void NVTX_EXT_MEM_VERSIONED_ID(nvtxExtMemInitOnce)
 #define NVTX_EXT_MEM_IMPL_FN_V1(ret_type, fn_name, signature, arg_names) \
 typedef ret_type (*fn_name##_impl_fntype)signature; \
 NVTX_DECLSPEC ret_type NVTX_API fn_name signature { \
+    NVTX_SET_NAME_MANGLING_OPTIONS \
     intptr_t* pSlot = &NVTX_EXT_MEM_VERSIONED_ID(nvtxExtMemSlots)[NVTX3EXT_CBID_##fn_name + 1]; \
     intptr_t slot = *pSlot; \
     if (slot != NVTX_EXTENSION_DISABLED) { \
