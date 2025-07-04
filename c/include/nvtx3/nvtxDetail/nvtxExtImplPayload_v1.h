e1ff3175171ffc5fda5fc5add4f5016051ade502
Robert Dietrich <rdietrich@nvidia.com>
2025-07-04 22:03:42 +0200

    Change default return value for nvtxDomainIsEnabled
    ... when a tool is attached but does not register for
    `nvtxDomainIsEnabled` to `1`.

--- c/include/nvtx3/nvtxDetail/nvtxExtImplPayload_v1.h
+++ c/include/nvtx3/nvtxDetail/nvtxExtImplPayload_v1.h
@@ -74,7 +74,35 @@ NVTX_LINKONCE_DEFINE_GLOBAL intptr_t
 NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxExtPayloadSlots)[NVTX_EXT_PAYLOAD_SLOT_COUNT + 1]
     = {0};
 
-/* Avoid warnings about missing prototype. */
+/* `NVTX_LINKONCE_FWDDECL_FUNCTION` is used to avoid warnings about missing prototypes. */
+
+/* This helper returns always `1` as `uint8_t`. */
+NVTX_LINKONCE_FWDDECL_FUNCTION uint8_t NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxReturnOne)(void);
+NVTX_LINKONCE_DEFINE_FUNCTION uint8_t NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxReturnOne)(void)
+{
+    return NVTX_STATIC_CAST(uint8_t, 1);
+}
+
+/*
+ * If a tool is attached, but does not handle `nvtxDomainIsEnabled`, the latter
+ * will always return `1` (enabled).
+ */
+NVTX_LINKONCE_FWDDECL_FUNCTION void NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxInitIsDomainEnabledFn)(void);
+NVTX_LINKONCE_DEFINE_FUNCTION void NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxInitIsDomainEnabledFn)(void)
+{
+    intptr_t* pSlot = &NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxExtPayloadSlots)[NVTX3EXT_CBID_nvtxDomainIsEnabled + 1];
+
+    /* The initialization disables all slots that have not been set by the tool.
+       This also happens if no tool is attached. If `injectionFnPtr` is set, a
+       tool is attached. A failng injection init function is not considered and
+       `nvtxDomainIsEnabled` will return `1`. */
+    if (*pSlot == NVTX_EXTENSION_DISABLED &&
+        NVTX_VERSIONED_IDENTIFIER(nvtxExtGlobals1).injectionFnPtr != NVTX_NULLPTR)
+    {
+        *pSlot = (intptr_t)NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxReturnOne);
+    }
+}
+
 NVTX_LINKONCE_FWDDECL_FUNCTION void NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxExtPayloadInitOnce)(void);
 NVTX_LINKONCE_DEFINE_FUNCTION void NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxExtPayloadInitOnce)(void)
 {
@@ -101,6 +129,8 @@ NVTX_LINKONCE_DEFINE_FUNCTION void NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxExtPayloadI
 
     NVTX_VERSIONED_IDENTIFIER(nvtxExtInitOnce)(&module_info,
         NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxExtPayloadSlots));
+
+    NVTX_EXT_PAYLOAD_VERSIONED_ID(nvtxInitIsDomainEnabledFn)();
 }
 
 #define NVTX_EXT_PAYLOAD_IMPL_FN_V1(ret_type, fn_name, signature, arg_names) \
