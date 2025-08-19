4160ea0006793aa0118ac1bed7361f82b496a4d8
Thomas Ilsche <tilsche@nvidia.com>
2025-08-19 15:43:10 +0200

    PayloadData: allow passing semantics header in helper macros

--- c/include/nvtx3/nvtxDetail/nvtxExtPayloadHelperInternal.h
+++ c/include/nvtx3/nvtxDetail/nvtxExtPayloadHelperInternal.h
@@ -82,9 +82,23 @@
     0, NVTX_PAYLOAD_ENTRY_##etype, name, desc, arraylen,
 #define _NVTX_PAYLOAD_SCHEMA_EF6(member, etype, name, desc, arraylen, flags) \
     NVTX_PAYLOAD_ENTRY_FLAG_##flags, NVTX_PAYLOAD_ENTRY_##etype, name, desc, arraylen,
+#define _NVTX_PAYLOAD_SCHEMA_EF7(member, etype, name, desc, arraylen, flags, semantics) \
+  NVTX_PAYLOAD_ENTRY_FLAG_##flags, NVTX_PAYLOAD_ENTRY_##etype, name, desc, arraylen,
 
 #define _NVTX_PAYLOAD_SCHEMA_ENTRY_FRONT(...) \
     NVTX_EXT_CONCAT(_NVTX_PAYLOAD_SCHEMA_EF, NVTX_EXT_NUM_ARGS(__VA_ARGS__))(__VA_ARGS__)
+
+#define _NVTX_PAYLOAD_SCHEMA_EB2(member, etype)                       NVTX_NULLPTR, NVTX_NULLPTR
+#define _NVTX_PAYLOAD_SCHEMA_EB3(member, etype, name)                 NVTX_NULLPTR, NVTX_NULLPTR
+#define _NVTX_PAYLOAD_SCHEMA_EB4(member, etype, name, desc)           NVTX_NULLPTR, NVTX_NULLPTR
+#define _NVTX_PAYLOAD_SCHEMA_EB5(member, etype, name, desc, arraylen) NVTX_NULLPTR, NVTX_NULLPTR
+#define _NVTX_PAYLOAD_SCHEMA_EB6(member, etype, name, desc, arraylen, flags) \
+  NVTX_NULLPTR, NVTX_NULLPTR
+#define _NVTX_PAYLOAD_SCHEMA_EB7(member, etype, name, desc, arraylen, flags, semantics) \
+  semantics, NVTX_NULLPTR
+
+#define _NVTX_PAYLOAD_SCHEMA_ENTRY_BACK(...) \
+  NVTX_EXT_CONCAT(_NVTX_PAYLOAD_SCHEMA_EB, NVTX_EXT_NUM_ARGS(__VA_ARGS__))(__VA_ARGS__)
 
 /* Second part of schema entry (append struct member).
    (At least two arguments are passed (`member` and `etype`). */
@@ -94,7 +108,7 @@
 #define _NVTX_PAYLOAD_SCHEMA_ENTRY(struct_id, entry) \
     {_NVTX_PAYLOAD_SCHEMA_ENTRY_FRONT entry \
     offsetof(struct_id, _NVTX_PAYLOAD_SCHEMA_ENTRY_END entry), \
-    NVTX_NULLPTR, NVTX_NULLPTR},
+    _NVTX_PAYLOAD_SCHEMA_ENTRY_BACK entry},
 
 /* Handle up to 16 schema entries. */
 #define _NVTX_PAYLOAD_SME1(s,e1)      _NVTX_PAYLOAD_SCHEMA_ENTRY(s,e1)
