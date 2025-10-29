7e55718ea490896ea2a2b3f9cc3c33bef1e9b0ad
Evan Ramos <evanr@nvidia.com>
2025-10-29 01:39:08 -0500

    CI: Add newly supported runner architectures

--- tests/scripts/build-windows-vs2022-x64.bat
+++ tests/scripts/build-windows-vs2022-x64.bat
@@ -7,9 +7,13 @@ cd %NAME%
 
 call "%VSPATH%\VC\Auxiliary\Build\vcvarsall.bat" x64
 
+set ENABLE_CUDA=False
+set NVCC=%CONDA%\envs\cuda-env\Library\bin\nvcc.exe
+if exist "%NVCC%" set ENABLE_CUDA=True
+
 set FLAGS=-Wall -wd4191 -wd4255 -wd4355 -wd4365 -wd4514 -wd4668 -wd4710 -wd4711 -wd4820 -wd5039 -wd5045 -wd5220 -WX -Zc:preprocessor
 
-"%VSPATH%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_COMPILER="%CONDA:\=/%/envs/cuda-env/Library/bin/nvcc.exe" -DCMAKE_C_FLAGS="-O2 %FLAGS%" -DCMAKE_CXX_FLAGS="-O2 %FLAGS%" -DCMAKE_CUDA_FLAGS="%FLAGS% -wd4555 --Wno-deprecated-gpu-targets --Werror all-warnings"
+"%VSPATH%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe" .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DENABLE_CUDA:BOOL="%ENABLE_CUDA%" -DCMAKE_CUDA_COMPILER="%NVCC:\=/%" -DCMAKE_C_FLAGS="-O2 %FLAGS%" -DCMAKE_CXX_FLAGS="-O2 %FLAGS%" -DCMAKE_CUDA_FLAGS="%FLAGS% -wd4555 --Wno-deprecated-gpu-targets --Werror all-warnings"
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 "%VSPATH%\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja\ninja.exe"
---
+++ tests/scripts/run-windows-vs2022-arm64_x64.bat
@@ -0,0 +1,12 @@
+@echo off
+pushd "%~dp0\.."
+
+set NAME=build-windows-vs2022-arm64_x64
+cd %NAME%
+
+call "%VSPATH%\VC\Auxiliary\Build\vcvarsall.bat" arm64_x64
+
+"%VSPATH%\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\ctest.exe" --output-on-failure
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+popd
