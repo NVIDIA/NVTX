89cb4dceff3fc3a010a96ad048c927c8f990f8d9
Evan Ramos <evanr@nvidia.com>
2025-11-04 12:24:06 -0600

    Connect correlation semantics header to tests and docs

--- tests/src/UseExportedApi.cpp
+++ tests/src/UseExportedApi.cpp
@@ -37,6 +37,7 @@
 #include <nvtx3/nvToolsExtSemanticsCounters.h>
 #include <nvtx3/nvToolsExtSemanticsScope.h>
 #include <nvtx3/nvToolsExtSemanticsTime.h>
+#include <nvtx3/nvToolsExtSemanticsCorrelation.h>
 #endif
 
 #include <nvtx3/nvtx3.hpp>
